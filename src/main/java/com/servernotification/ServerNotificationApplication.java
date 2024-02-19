080df2ffee49500267788f654e058febf2344136
hak <hak404notfound@gmail.com>
2024-02-19 13:28:45 +0700

    started server

---
+++ src/main/java/com/servernotification/ServerNotificationApplication.java
@@ -0,0 +1,14 @@
+package com.servernotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerNotificationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ServerNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servernotification/ServerNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servernotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
